01303094f60e23b5efa5f36c2605e9a1fd3ecc27
Lucas Fouraux <lucasfouraux@gmail.com>
2021-09-18 08:09:04 -0300

    refactor: connect to mongodb before integration tests

--- src/main/routes/signup.routes.test.ts
+++ src/main/routes/signup.routes.test.ts
@@ -2,6 +2,19 @@ import request from 'supertest'
 import app from '../config/app'
 
 describe('SignUp Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   it('should return an account on success CORS', async () => {
     await request(app)
       .post('/api/signup')
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,3 @@
 import app from './config/app'
 
-app.listen(3333, () => console.log('Server running att http://localhost:5050'))
+app.listen(3000, () => console.log('Server running att http://localhost:3000'))
