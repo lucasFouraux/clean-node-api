12651fcbb128dce43464150adf0e94102f010034
Lucas Fouraux <lucasfouraux@gmail.com>
2021-10-29 19:47:26 -0300

    test: login route returns 401 with invalid credentials

--- src/main/routes/login.routes.test.ts
+++ src/main/routes/login.routes.test.ts
@@ -51,5 +51,15 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+
+    it('should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'lucasfouraux@gmail.com',
+          password: '123'
+        })
+        .expect(401)
+    })
   })
 })
