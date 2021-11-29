3ff930dcf2fa14a1eca39a8376171806c4a12f63
Lucas Fouraux <lucasfouraux@gmail.com>
2021-11-29 09:56:45 -0300

    test: ensure AccountMongoRepository will not find an account if find account by token fails

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -117,10 +117,10 @@ describe('Account Mongo Repository', () => {
       expect(account?.password).toBe('any_password')
     })
 
-    // it('should return null if loadByEmail fails', async () => {
-    //   const sut = makeSut()
-    //   const account = await sut.loadByEmail('any_email@mail.com')
-    //   expect(account).toBeFalsy()
-    // })
+    it('should return null if loadByToken fails', async () => {
+      const sut = makeSut()
+      const account = await sut.loadByToken('any_token')
+      expect(account).toBeFalsy()
+    })
   })
 })
