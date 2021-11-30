bd445b09d9aa72ea0d1d60f82a50235afad13f02
Lucas Fouraux <lucasfouraux@gmail.com>
2021-11-30 14:26:54 -0300

    test: ensure add survey route returns 403 if no access token is provided

--- src/main/routes/survey.routes.test.ts
+++ src/main/routes/survey.routes.test.ts
@@ -20,7 +20,7 @@ describe('Survey Routes', () => {
   })
 
   describe('POST /surveys', () => {
-    it('should return 204 on add survey success', async () => {
+    it('should return 403 on add survey success', async () => {
       await request(app)
         .post('/api/surveys')
         .send({
@@ -32,7 +32,7 @@ describe('Survey Routes', () => {
             answer: 'Answer 2'
           }]
         })
-        .expect(204)
+        .expect(403)
     })
   })
 })
