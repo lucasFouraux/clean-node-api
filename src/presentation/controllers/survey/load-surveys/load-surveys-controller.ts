596045b96ce890244d6739c44a3eb51cc30852ab
Lucas Fouraux <lucasfouraux@gmail.com>
2021-12-02 22:22:51 -0300

    feat: ensure LoadSurveysController calls LoadSurveysUseCase

---
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
@@ -0,0 +1,11 @@
+import { ok } from '../../../helpers/http/http-helper'
+import { HttpRequest, HttpResponse } from '../add-survey/add-survey-controller-protocols'
+import { Controller, LoadSurveys } from './load-surveys-controller-protocols'
+
+export class LoadSurveysController implements Controller {
+  constructor (private readonly loadSurveys: LoadSurveys) {}
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    await this.loadSurveys.load()
+    return ok({})
+  }
+}
