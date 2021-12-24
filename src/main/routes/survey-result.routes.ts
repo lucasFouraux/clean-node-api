99634236bc80335f488f1aae264f8354fd1626e0
Lucas Fouraux <lucasfouraux@gmail.com>
2021-12-23 21:42:03 -0300

    refactor: change route /surveys/:surveyId/result to PUT

--- src/main/routes/survey-result.routes.ts
+++ src/main/routes/survey-result.routes.ts
@@ -4,5 +4,5 @@ import { auth } from '@/main/middlewares/auth'
 import { makeSaveSurveyResultController } from '@/main/factories/controllers/survey-result/save-survey-result/save-survey-result-controller-factory'
 
 export default (router: Router): void => {
-  router.post('/surveys/:surveyId/results', auth, adaptRoute(makeSaveSurveyResultController()))
+  router.put('/surveys/:surveyId/results', auth, adaptRoute(makeSaveSurveyResultController()))
 }
