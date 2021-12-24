4073f3462c12d9d0d55f3caa04853ef2625b4034
Lucas Fouraux <lucasfouraux@gmail.com>
2021-12-23 21:36:21 -0300

    Revert "feat: ensure save survey result route returns 403 if no token is provided"
    This reverts commit 76475fcfcaf7c6a3c76721b7b5e0fcae62176524.

--- src/main/routes/survey-result.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-import { adaptRoute } from '@/main/adapters/express-route-adapter'
-import { auth } from '@/main/middlewares/auth'
-import { makeSaveSurveyResultController } from '@/main/factories/controllers/survey-result/save-survey-result/save-survey-result-controller-factory'
-
-export default (router: Router): void => {
-  router.post('/surveys/:surveyId/results', auth, adaptRoute(makeSaveSurveyResultController()))
-}
