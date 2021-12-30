278603d11a852a71ea189e237854a6eb6f5b745a
Lucas Fouraux <lucasfouraux@gmail.com>
2021-12-30 14:19:16 -0300

    chore: add swagger

--- src/data/usecases/survey/add-survey/db-add-survey.spec.ts
+++ src/data/usecases/survey/add-survey/db-add-survey.spec.ts
@@ -38,7 +38,7 @@ describe('DbAddSurvey Usecase', () => {
 
   it('should throw if AddSurveyRepository throws', async () => {
     const { sut, addSurveyRepositoryStub } = makeSut()
-    jest.spyOn(addSurveyRepositoryStub, 'add').mockImplementationOnce(async () => await new Promise((resolve, reject) => reject(new Error())))
+    jest.spyOn(addSurveyRepositoryStub, 'add').mockReturnValueOnce(Promise.reject(new Error()))
     const promise = sut.add(mockAddSurveyParams())
     await expect(promise).rejects.toThrow()
   })
