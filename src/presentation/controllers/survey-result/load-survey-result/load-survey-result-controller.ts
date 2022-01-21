cb1ffe04c6d4ef8f7f756e3f5e7bc31657a27982
Lucas Fouraux <lucasfouraux@gmail.com>
2022-01-21 17:31:18 -0300

    Revert "feat: ensure LoadSurveyResultController returns 403 if an invalid surveyId is provided"
    This reverts commit b6c567f3ba30b3130a694798eb2eecf625582db3.

--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
@@ -1,15 +1,11 @@
-import { InvalidParamError } from '@/presentation/errors'
-import { forbidden, ok } from '@/presentation/helpers/http/http-helper'
+import { ok } from '@/presentation/helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse, LoadSurveyById } from './load-survey-result-controller-protocols'
 
 export class LoadSurveyResultController implements Controller {
   constructor (private readonly loadSurveyById: LoadSurveyById) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const survey = await this.loadSurveyById.loadById(httpRequest.params.surveyId)
-    if (!survey) {
-      forbidden(new InvalidParamError('surveyId'))
-    }
+    await this.loadSurveyById.loadById(httpRequest.params.surveyId)
     return await Promise.resolve(ok({}))
   }
 }
