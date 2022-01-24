3150c35220216cb44575c9bf6e7e31137f7cc55e
Lucas Fouraux <lucasfouraux@gmail.com>
2022-01-24 17:29:12 -0300

    feat: ensure load survey route returns 403 if no accessToken is provided

---
+++ src/main/factories/controllers/survey-result/load-survey-result/load-survey-result-controller-factory copy.ts
@@ -0,0 +1,8 @@
+import { makeDbLoadSurveyResult } from '@/main/factories/usecases/survey-result/load-survey-result/db-load-survey-result-factory'
+import { makeDbLoadSurveybyId } from '@/main/factories/usecases/survey/load-survey-by-id/db-load-survey-by-id-factory'
+import { LoadSurveyResultController } from '@/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller'
+import { Controller } from '@/presentation/protocols'
+
+export const makeLoadSurveyResultController = (): Controller => {
+  return new LoadSurveyResultController(makeDbLoadSurveybyId(), makeDbLoadSurveyResult())
+}
