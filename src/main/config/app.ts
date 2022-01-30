497a1f92a47a08f957f5a823871129278a10d3c7
Lucas Fouraux <lucasfouraux@gmail.com>
2022-01-30 12:08:25 -0300

    feat: add route to serve static files

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,11 +1,12 @@
 import setupMiddlwares from './middlewares'
 import setupRoutes from './routes'
+import setupStaticFiles from './statis-files'
 import setupSwagger from './config-swagger'
 import express from 'express'
 
 const app = express()
+setupStaticFiles(app)
 setupSwagger(app)
 setupMiddlwares(app)
 setupRoutes(app)
-
 export default app
